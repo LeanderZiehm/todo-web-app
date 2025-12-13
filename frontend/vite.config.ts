5a15e8cffd3a052b9990d17c7aca860e3e49206b
LeanderZiehm <leander-arun.ziehm@stud.th-deg.de>
2025-12-13 15:24:36 +0100

    refactor(frontend): remove dev dependency vite-plugin-pwa by just using manifest service worker and workbox directly

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,23 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import {VitePWA } from "vite-plugin-pwa";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(),
-    VitePWA(
-      {manifest:{
-        icons:[
-          {
-            src:"todo-icon.png",//folder: public/ 
-            sizes: "512x512",
-            type: "image/png",
-            purpose: "any maskable"
-          }
-        ]
-      }
-        
-      }
-    )
-  ],
+  plugins: [react()],
 })
